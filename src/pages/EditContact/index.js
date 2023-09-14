87bd2d44fa710d8c084f0040a25a213b82ce75e1
Rodrigo Gama <rodrigongama.adv@gmail.com>
2023-09-14 08:55:48 -0300

    feat: edit contact page

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,12 +1,81 @@
+import { useEffect, useRef, useState } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
+import Loader from '../../components/Loader';
+import useSafeAsyncAction from '../../hooks/useSafeAsyncAction';
+
+import ContactsService from '../../services/ContactsService';
+import toast from '../../utils/toast';
 
 export default function EditContact() {
+  const { id } = useParams();
+  const history = useHistory();
+  const contactFormRef = useRef(null);
+  const safeAsyncAction = useSafeAsyncAction();
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [contactName, setContactName] = useState('');
+
+  async function handleSubmit(formData) {
+    try {
+      const updatedContact = await ContactsService.updateContact(id, {
+        ...formData,
+        category_id: formData.categoryId,
+      });
+
+      setContactName(updatedContact.name);
+
+      toast({
+        type: 'success',
+        text: 'Contato editado com sucesso!',
+      });
+    } catch (error) {
+      toast({
+        type: 'danger',
+        text: 'Ocorreu um erro ao editar o contato!',
+      });
+    }
+  }
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contact = await ContactsService.getContactById(id);
+
+        safeAsyncAction(() => {
+          contactFormRef.current.setFieldsValues(contact);
+          setContactName(contact.name);
+          setIsLoading(false);
+        });
+      } catch {
+        history.push('/');
+        safeAsyncAction(() => {
+          toast({
+            type: 'danger',
+            text: 'Contato não encontrado!',
+          });
+        });
+      }
+    }
+
+    loadContact();
+  }, [id, history, safeAsyncAction]);
+
   return (
     <>
-      <PageHeader title="Editar contato" />
+      <Loader isLoading={isLoading} />
 
-      <ContactForm buttonLabel="Salvar alterações" />
+      <PageHeader
+        title={isLoading ? 'Carregando...' : `Editar contato ${contactName}`}
+      />
+
+      <ContactForm
+        ref={contactFormRef}
+        buttonLabel="Salvar alterações"
+        onSubmit={handleSubmit}
+      />
     </>
   );
 }
