57c0e099ab3bbd1dd01409a631c98d90b597668e
Rodrigo Gama <rodrigongama.adv@gmail.com>
2023-09-14 21:22:59 -0300

    feat: adding backend env

--- src/services/CategoriesService.js
+++ src/services/CategoriesService.js
@@ -3,7 +3,7 @@ import CategoryMapper from './mappers/CategoryMapper';
 
 class CategoriesService {
   constructor() {
-    this.httpClient = new HttpClient('http://localhost:3001');
+    this.httpClient = new HttpClient(process.env.REACT_APP_BACKEND_URL);
   }
 
   async listCategories() {
--- src/services/ContactsService.js
+++ src/services/ContactsService.js
@@ -3,7 +3,7 @@ import ContactMapper from './mappers/ContactMapper';
 
 class ContactService {
   constructor() {
-    this.httpClient = new HttpClient('http://localhost:3001');
+    this.httpClient = new HttpClient(process.env.REACT_APP_BACKEND_URL);
   }
 
   async listContacts(orderBy = 'asc') {
