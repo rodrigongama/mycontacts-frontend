86d9e91fc72544c8fe0dbc09dafd36aba71cc837
Rodrigo Gama <rodrigongama.adv@gmail.com>
2023-09-20 17:38:52 -0300

    feat: deferring filtered contacts update

--- src/pages/Home/useHome.js
+++ src/pages/Home/useHome.js
@@ -1,20 +1,33 @@
-import { useEffect, useMemo, useState } from 'react';
+import {
+  useEffect, useState, useMemo, useCallback, useDeferredValue,
+} from 'react';
 import toast from '../../utils/toast';
 import ContactsService from '../../services/ContactsService';
 
 export default function useHome() {
   const [contacts, setContacts] = useState([]);
   const [orderBy, setOrderBy] = useState('asc');
-  const [searchTerm, setSearchTerm] = useState('');
   const [isLoading, setIsLoading] = useState(true);
   const [hasError, setHasError] = useState(false);
   const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false);
   const [contactBeingDeleted, setContactBeingDeleted] = useState(null);
   const [isLoadingDelete, setIsLoadingDelete] = useState(false);
 
+  const [searchTerm, setSearchTerm] = useState('');
+  const deferredSearchTerm = useDeferredValue(searchTerm);
+
   const filteredContacts = useMemo(() => contacts.filter((contact) => (
-    contact.name.toLowerCase().includes(searchTerm.toLowerCase())
-  )), [contacts, searchTerm]);
+    contact.name.toLowerCase().includes(deferredSearchTerm.toLowerCase())
+  )), [contacts, deferredSearchTerm]);
+
+  const handleToggleOrderBy = useCallback(() => {
+    setOrderBy((prevState) => (prevState === 'asc' ? 'desc' : 'asc'));
+  }, []);
+
+  const handleDeleteContact = useCallback((contact) => {
+    setContactBeingDeleted(contact);
+    setIsDeleteModalVisible(true);
+  }, []);
 
   async function loadContacts(order) {
     try {
@@ -32,21 +45,12 @@ export default function useHome() {
     }
   }
 
-  function handleToggleOrderBy() {
-    setOrderBy((prevState) => (prevState === 'asc' ? 'desc' : 'asc'));
-  }
-
   function handleChangeSearchTerm(event) {
     setSearchTerm(event.target.value);
   }
 
   function handleTryAgain() {
     loadContacts();
-  }
-
-  function handleDeleteContact(contact) {
-    setContactBeingDeleted(contact);
-    setIsDeleteModalVisible(true);
   }
 
   function handleCloseDeleteModal() {
